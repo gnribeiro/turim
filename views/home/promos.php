b7350271b8203d4f9dc0840cdaec04cd60bb4484
Gonçalo Ribeiro <goncalo.ribeiro@passworks.io>
2016-02-05 15:11:41 +0000

    homepage2

--- views/home/promos.php
+++ views/home/promos.php
@@ -1,6 +1,14 @@
+<?php  global $site?>
 <section class="a-dark-ligth p-b-65">
     <div class="a-container">
+    <?php
+        if (count($destaques)){
+            foreach ($destaques as $value){
+                $view = 'home/destaques/'.$value['color_highlight'];
+                echo $site->get_partial($view, array('data' , $value));
+            }
+        }
+    ?>
 
-        <?php pr($destaques) ?>
     </div>
 </section>
